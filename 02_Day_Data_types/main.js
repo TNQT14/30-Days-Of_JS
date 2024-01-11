60bd50320a218e3e265b594f1ea2fb0765dd2975
Trương Nguyễn Quang Thái <87488896+TNQT14@users.noreply.github.com>
2024-01-11 17:48:42 +0700

    Day 2

---
+++ 02_Day_Data_types/main.js
@@ -0,0 +1,8 @@
+// Let us access the first character in 'JavaScript' string.
+
+// charAt(): Takes index and it returns the value at that index
+string.charAt(index)
+let string = '30 Days Of JavaScript'
+console.log(string.charAt(0)) // 3
+let lastIndex = string.length - 1
+console.log(string.charAt(lastIndex)) // t
